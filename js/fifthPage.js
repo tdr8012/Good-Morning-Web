6578ba07a7dc01fe588297d83fcedb4fcb1875cc
Thai Duy Roan <128533558+tdr8012@users.noreply.github.com>
2025-07-11 19:34:09 -0400

    fix page 5 js

--- js/fifthPage.js
+++ js/fifthPage.js
@@ -1,20 +1,24 @@
 window.addEventListener("load", () => {
-    document.body.classList.add("loaded");
+  document.body.classList.add("loaded");
 });
-
 
 const envelopeWrapper = document.querySelector('.envelope-wrapper');
 const heart = document.querySelector('.heart');
 const subtitleP = document.querySelector('.subtitle p');
-
+const letter = document.querySelector('.letter');
 
 envelopeWrapper.classList.add('shake');
 
 heart.addEventListener('click', toggleEnvelope);
 heart.addEventListener('touchstart', toggleEnvelope);
 
+
+letter.addEventListener('click', toggleEnvelope);
+letter.addEventListener('touchstart', toggleEnvelope);
+
 function toggleEnvelope(e) {
-  e.preventDefault(); 
+  e.preventDefault();
+
   envelopeWrapper.classList.toggle('flap');
 
   if (envelopeWrapper.classList.contains('flap')) {
